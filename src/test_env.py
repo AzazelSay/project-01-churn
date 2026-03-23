6ef20a903c8c53712cf51935b334a58596b1b8d1
Твоё имя <azat2000azat00@mail.ru>
2026-03-23 17:56:35 +0300

    initial project setup

---
+++ src/test_env.py
@@ -0,0 +1,7 @@
+import sys
+import pandas as pd
+import sklearn
+
+print("Python executable:", sys.executable)
+print("Pandas executable:", pd.__version__)
+print("Sklearn executable:", sklearn.__version__)
